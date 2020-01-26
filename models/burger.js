c2109dbfda1ab88b3d7088d1e4884e344eb0461d
e-holmes <bethholmes768@gmail.com>
2020-01-25 22:14:22 -0800

    Began burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,33 @@
 // In models, make a burger.js file.
-
+var orm = require("../config/orm.js");
 
 // Inside burger.js, import orm.js into burger.js
+var  burger= {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
 
-
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+// Also inside burger.js, create the code that will call the ORM functions
+//  using burger specific input for the ORM.
+create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
 
 // Export at the end of the burger.js file.
+module.exports = burger;
