2ea645b0fc8f49be45379bdfbfb2dfea83596f11
e-holmes <bethholmes768@gmail.com>
2020-01-25 22:01:40 -0800

    config began

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,77 @@
-// Create an orm.js file inside config directory.
-
-
 // Import (require) connection.js into orm.js
+var connection = require("./connection.js");
 
+// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. 
+// These are the methods you will need to use in order to retrieve and store data in your database.
 
-// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
 // selectAll()
+var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
 // insertOne()
-// updateOne()
+create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+
+
+// updateOne()
+update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
 
 
 // Export the ORM object in module.exports.
+module.exports = orm;
