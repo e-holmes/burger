6bc7aefaf5acfe580d643bdbb2d40ca260d6681a
e-holmes <bethholmes768@gmail.com>
2020-01-25 22:12:36 -0800

    fixed db- id

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,17 @@
 -- In the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
-DROP DATABASE IF EXISTS top_songsDB;
+DROP DATABASE IF EXISTS burgers_db;
 -- Create the burgers_db.
-CREATE database top_songsDB;
+CREATE database burgers_db;
 
 
 -- Switch to or use the burgers_db.
-USE top_songsDB;
+USE burgers_db;
 
 
 -- Create a burgers table with these fields:
 CREATE TABLE burgers (
     -- id: an auto incrementing int that serves as the primary key.
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     -- burger_name: a string.
     burger_name VARCHAR (100) NULL,
     -- devoured: a boolean.
